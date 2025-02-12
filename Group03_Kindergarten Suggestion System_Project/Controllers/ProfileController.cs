04e711db7062f29d95e1c1107abce5b8905f1e68
QuangVu-Dev2003 <vunuong003@gmail.com>
2025-02-12 09:42:27 +0700

    updateUI

---
+++ Group03_Kindergarten Suggestion System_Project/Controllers/ProfileController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Group03_Kindergarten_Suggestion_System_Project.Controllers
+{
+    public class ProfileController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }   
+        
+        public IActionResult ChangePassword()
+        {
+            return View();
+        }   
+        
+        public IActionResult EditProfile()
+        {
+            return View();
+        }
+    }
+}
---
+++ Group03_Kindergarten Suggestion System_Project/Helpers/ActiveMenuHelper.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Group03_Kindergarten_Suggestion_System_Project.Helpers
+{
+    public static class ActiveMenuHelper
+    {
+        public static string IsActive(this IHtmlHelper htmlHelper, string controller, params string[] actions)
+        {
+            var routeData = htmlHelper.ViewContext.RouteData;
+            var currentController = routeData.Values["controller"]?.ToString();
+            var currentAction = routeData.Values["action"]?.ToString();
+            return (controller == currentController && actions.Contains(currentAction)) ? "active" : string.Empty;
+        }
+
+        public static string IsActiveParent(this IHtmlHelper htmlHelper, params string[] controllers)
+        {
+            var routeData = htmlHelper.ViewContext.RouteData;
+            var currentController = routeData.Values["controller"]?.ToString();
+            return controllers.Contains(currentController) ? "active" : string.Empty;
+        }
+    }
+}
